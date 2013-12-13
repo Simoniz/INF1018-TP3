eda952f88c38ffce5371c26ac74d9f39e9896a87
wheeliez <mathieu.dargis@gmail.com>
2013-12-13 15:42:24 -0500

    Ajout du fichier Type.java

---
+++ Type.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+
+public class Type {
+	public static final String BOOLEAN = "boolean";
+	public static final String CHAR = "char";
+	public static final String BYTE = "byte";
+	public static final String SHORT = "short";
+	public static final String INT = "int";
+	public static final String LONG = "long";
+	public static final String FLOAT = "float";
+	public static final String DOUBLE = "double";
+	
+	private ArrayList<String> listTypes;
+	
+	public Type() {
+		this.listTypes = new ArrayList<String>();
+		this.listTypes.add(Type.BOOLEAN);
+		this.listTypes.add(Type.CHAR);
+		this.listTypes.add(Type.BYTE);
+		this.listTypes.add(Type.SHORT);
+		this.listTypes.add(Type.INT);
+		this.listTypes.add(Type.LONG);
+		this.listTypes.add(Type.FLOAT);
+		this.listTypes.add(Type.DOUBLE);
+	}
+	
+	public boolean contains(String _type) {
+		return listTypes.contains(_type);
+	}
+}
